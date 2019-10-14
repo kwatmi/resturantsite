af0fcece104ca5a9d37087d4d1e48eac38b8d7af
Kwatmi Haruna Paul <kwatmiharunapaul@gmail.com>
2019-10-14 16:55:57 +0100

    map wrapper section added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MapWrapper from './components/MapWrapper'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='appWrapper'>
+       <div className='innerWrapper'>
+         
+
+        <MapWrapper 
+        handleDetailRequest={this.handleDetailRequest}
+        places={this.state.places}
+        pos={this.state.pos} 
+        handleRecentre={this.handleRecentre}
+        handleDrag={this.handleDrag} 
+        handlePlaces={this.handlePlaces}
+        handleMarkers={this.handleMarkers}
+        handleMap={this.handleMap}
+        markers={this.state.markers}
+        map={this.state.map}
+        currentPlace={this.currentPlace}
+        storedDetails={this.state.storedDetails}
+        handleStoredDetails={this.handleStoredDetails}
+       
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+         
+        </MapWrapper>
+       </div>
+     </div> 
   );
 }
 
