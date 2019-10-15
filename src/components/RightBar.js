3ad5d80fdf42516ee0eb914846977ed77ed4b4b2
Kwatmi Haruna Paul <kwatmiharunapaul@gmail.com>
2019-10-15 12:10:28 +0100

    list items added to right side bar

--- src/components/RightBar.js
+++ src/components/RightBar.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import ListItem from './ListItem'
+
 class RightBar extends React.Component{
     render() {
         return (
-            )
-  }
-}
+            <div className='rightWrapper'>
+                <div className="listWrapper">
+                {this.props.places.map(place => {
+                
+                    return(
+                    < ListItem 
+                        place={place} 
+                        storedDetails={this.props.storedDetails}
+                        handleStoredDetails={this.props.handleStoredDetails}
+                        key={place.place_id} 
+                        id={place.place_id} 
+                        markers={this.props.markers} 
+                        handleDetailRequest={this.props.handleDetailRequest} 
+                        map={this.props.map}
+                        currentPlace={this.props.currentPlace}
+                    />
+                    )})}
+                </div>
+            </div>
+            )}}
 
 export default RightBar;
